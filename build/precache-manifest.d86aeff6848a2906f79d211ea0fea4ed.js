9f81f3eca235586482a78f3b4eaaffdc571e1923
Avinash-S95 <avinash@crayond.co>
2020-03-25 19:10:23 +0530

    build folder

---
+++ build/precache-manifest.d86aeff6848a2906f79d211ea0fea4ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab54a3db83535a368c5ff95737dcef0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5c9f47541afe12b3389",
+    "url": "/static/css/main.1db553f1.chunk.css"
+  },
+  {
+    "revision": "47a4869d8aa932eaeb80",
+    "url": "/static/js/2.f8543c2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f8543c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5c9f47541afe12b3389",
+    "url": "/static/js/main.114a4d4f.chunk.js"
+  },
+  {
+    "revision": "eb81441c62b43b4493e5",
+    "url": "/static/js/runtime-main.543e3e97.js"
+  },
+  {
+    "revision": "59bfd069854bd41d23dc32c177dc7538",
+    "url": "/static/media/Logo.59bfd069.jpeg"
+  }
+]);
